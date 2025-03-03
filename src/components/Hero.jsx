681293e2c52ea309ccd534036e726a9074390a5d
Reece Peardon <peardon.reece@gmail.com>
2025-03-02 22:09:28 -0800

    scrolltrigger impleneted fully, changed clippath values a bit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,8 @@ import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Hero = () => {
 const [currentIndex, setCurrentIndex] = useState(1)
 const [hasClicked, setHasClicked] = useState(false)
@@ -55,7 +57,7 @@ const upcomingVideoIndex = (currentIndex % totalVideos) + 1
 
   useGSAP(() => {
     gsap.set('#video-frame', {
-      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      clipPath: 'polygon(14% 0%, 72% 0%, 88% 90%, 0% 95%)',
       bordeerRadius: '0 0 40% 10%'
     })
     gsap.from('#video-frame', {
