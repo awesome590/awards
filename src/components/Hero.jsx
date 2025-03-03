716643e57321977f56a53460dfcc73a163dd7173
Reece Peardon <peardon.reece@gmail.com>
2025-03-02 22:06:21 -0800

    implemented loading animation when page loads

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
 import { useGSAP } from "@gsap/react"
@@ -25,6 +25,12 @@ const upcomingVideoIndex = (currentIndex % totalVideos) + 1
 
     setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1)
   }
+
+  useEffect(() => {
+    if(loadedVideos === totalVideos -1) {
+      setIsLoading(false)
+    }
+  }, [loadedVideos])
 
   useGSAP(() => {
     if(hasClicked) {
@@ -64,10 +70,20 @@ const upcomingVideoIndex = (currentIndex % totalVideos) + 1
       }
     })
   })
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
       <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden
       rounded-lg bg-blue-75">
         <div>
