ff0c05a66944bfab742db8d7a2be349f3f64400e
Reece Peardon <peardon.reece@gmail.com>
2025-03-02 22:00:38 -0800

    started animation for video croping when scrolling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all"
 
 const Hero = () => {
 const [currentIndex, setCurrentIndex] = useState(1)
@@ -46,6 +47,23 @@ const upcomingVideoIndex = (currentIndex % totalVideos) + 1
     }
   }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      bordeerRadius: '0 0 40% 10%'
+    })
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      }
+    })
+  })
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
   return (
