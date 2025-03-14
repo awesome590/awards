187cc05667fdb819f20a68c6ce3656b8d6182452
Reece Peardon <peardon.reece@gmail.com>
2025-03-13 22:27:37 -0700

    implenented moving animation when user mouses over the image in story section and rounded the corners of the image

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,14 +1,40 @@
 import React, { useRef } from 'react'
+import gsap from 'gsap'
 import AnimatedTitle from './AnimatedTitle'
+import RoundedCorners from './RoundedCorners'
 
 const Story = () => {
   const frameRef = useRef('null')
-
-  const handleMouseLeave = () => {}
-  const handleMouseMove = (e) => {
-
+  const handleMouseLeave = () => {
+    const element = frameRef.current
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      transformPerspective: 500,
+      ease: 'power1.inOut'
+    })
   }
-  
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    const element = frameRef.current
+    if(!e) return
+    const rect = element.getBoundingClientRect()
+    const x = clientX - rect.left
+    const y = clientY - rect.top
+    const centerX = rect.width / 2
+    const centerY = rect.height / 2
+    const rotateX = ((y - centerY) / centerY) * -10
+    const rotateY =  ((x - centerX) / centerX) * 10
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX, rotateY,
+      transformPerspective: 500,
+      ease: 'power1.inOut'
+    })
+  }
+
   return (
     <section id="story" className='min-h-dvh w-screen bg-black text-blue-50'>
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -33,6 +59,7 @@ const Story = () => {
                 className='object-container' />
               </div>
             </div>
+            <RoundedCorners />
           </div>
         </div>
       </div>
